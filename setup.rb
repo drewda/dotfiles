b301ab864153f6c7e4c0efe3d8ef1734c6732f7c
Drew Dara-Abrams <drew@DDAMBP.local>
2011-08-25 14:24:45 -0700

    initial versions of dot files
    * .gitconfig help from
    http://segfault.in/2010/03/whats-in-your-gitconfig/ * sync script from
    http://minhajuddin.com/2011/05/03/painless-dotfiles-synchronization-and-versioning-using-git/
    * .vimrc help from
    http://stackoverflow.com/questions/164847/what-is-in-your-vimrc/652532#652532

---
+++ setup.rb
@@ -0,0 +1,10 @@
+#!/bin/env ruby
+dirs = %w(bash/.bash_aliases bash/.inputrc git/.gitconfig vim/.vim vim/.vimrc .gemrc)
+current_dir = File.expand_path(Dir.pwd)
+home_dir = File.expand_path("~")
+ 
+dirs.each do |dir|
+  dir = File.join(current_dir, dir)
+  symlink = File.join(home_dir, File.basename(dir))
+  `ln -ns #{dir} #{symlink}`
+end
