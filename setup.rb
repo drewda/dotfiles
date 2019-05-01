5a2760db7dab91ff1582b7c9f2f4c8c1724185aa
Drew Dara-Abrams <dda@dara-abrams.com>
2019-05-01 14:50:49 -0700

    add Bitbar plugins

--- setup.rb
+++ setup.rb
@@ -31,7 +31,6 @@ end
 
 vscode_user_dir = File.join(home_dir, 'Library/Application Support/Code/User')
 vscode_config_dir = 'vscode'
-
 Dir.foreach(vscode_config_dir).each do |dir|
   dir = File.join(current_dir, vscode_config_dir, dir)
   symlink = File.join(vscode_user_dir, File.basename(dir))
@@ -52,4 +51,14 @@ else
   puts "Symlinking: #{symlink}"
   iterm2_config_file = File.join(current_dir, iterm2_config_file_name)
   `ln -ns #{iterm2_config_file} #{symlink}`
+end
+
+bitbar_user_dir = File.join(home_dir, '.bitbar')
+bitbar_config_dir = File.join(current_dir, 'bitbar')
+if File.exist?(bitbar_user_dir)
+  puts "Directory already exists: #{bitbar_user_dir}"
+else
+  bitbar_user_dir.gsub!(' ', '\ ')
+  puts "Symlinking: #{bitbar_user_dir}"
+  `ln -ns #{bitbar_config_dir} #{bitbar_user_dir}`
 end
